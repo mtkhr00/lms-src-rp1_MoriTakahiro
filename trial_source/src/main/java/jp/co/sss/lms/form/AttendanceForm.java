3231ef4a983bbf9d12542404306a831e9868b194
user <user@WIN-NDAPNIHP71N>
2026-09-15 17:55:49 +0900

    fix:森 Task.26 AttendanceForm.javaの修正
    出勤時間と退勤時間の時間と分を保持するためのフィールドを追加

--- trial_source/src/main/java/jp/co/sss/lms/form/AttendanceForm.java
+++ trial_source/src/main/java/jp/co/sss/lms/form/AttendanceForm.java
@@ -29,6 +29,18 @@ public class AttendanceForm {
 	private String dispLeaveDate;
 	/** 中抜け時間(プルダウン) */
 	private LinkedHashMap<Integer, String> blankTimes;
+	
+	/*-- 森 Task.26 --*/
+	/** 出勤時間(時)(プルダウン) */
+	private LinkedHashMap<Integer, String> trainingStartTimeHour;
+	/** 出勤時間(分)(プルダウン) */
+	private LinkedHashMap<Integer, String> trainingStartTimeMinute;
+	/** 退勤時間(時)(プルダウン) */
+	private LinkedHashMap<Integer, String> trainingEndTimeHour;
+	/** 退勤時間(分)(プルダウン) */
+	private LinkedHashMap<Integer, String> trainingEndTimeMinute;
+	/*-- 森 Task.26 --*/
+	
 	/** 日次の勤怠フォームリスト */
 	private List<DailyAttendanceForm> attendanceList;
 
