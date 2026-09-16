6391c7cc0e6c5c654e02dedb5abef2cf8931ad76
user <user@WIN-NDAPNIHP71N>
2026-09-16 16:22:26 +0900

    fix:森 Task.26 DailyAttendanceFormの修正
    新たにフィールドを追加。

--- trial_source/src/main/java/jp/co/sss/lms/form/DailyAttendanceForm.java
+++ trial_source/src/main/java/jp/co/sss/lms/form/DailyAttendanceForm.java
@@ -20,6 +20,18 @@ public class DailyAttendanceForm {
 	private String trainingStartTime;
 	/** 退勤時間 */
 	private String trainingEndTime;
+	
+	/*-- 森 Task.26 --*/
+	/** 出勤時間(時) */
+	private Integer trainingStartTimeHour;
+	/** 出勤時間(分) */
+	private Integer trainingStartTimeMinute;
+	/** 退勤時間(時) */
+	private Integer trainingEndTimeHour;
+	/** 退勤時間(分) */
+	private Integer trainingEndTimeMinute;
+	/*-- 森 Task.26 --*/
+	
 	/** 中抜け時間 */
 	private Integer blankTime;
 	/** 中抜け時間（画面表示用） */
